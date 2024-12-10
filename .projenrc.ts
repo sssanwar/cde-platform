d23c2e2e7f9b09d1c64d957b13e24ed9fbd14adf
Stephen Anwar <stephen.anwar@gmail.com>
2024-12-10 16:01:32 +1100

    build: Use pnpm

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,5 +1,26 @@
-import { typescript } from 'projen';
+import { awscdk, typescript } from 'projen'
+import { ArrowParens, NodePackageManager } from 'projen/lib/javascript'
+
+const baseOptions = {
+  minNodeVersion: '22',
+  packageManager: NodePackageManager.PNPM,
+  pnpmVersion: '9.15.0',
+  defaultReleaseBranch: 'main',
+  requireApproval: awscdk.ApprovalLevel.NEVER,
+  depsUpgrade: false,
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 130,
+      singleQuote: true,
+      semi: false,
+      arrowParens: ArrowParens.AVOID,
+    },
+  },
+}
+
 const project = new typescript.TypeScriptProject({
+  ...baseOptions,
   defaultReleaseBranch: 'main',
   name: 'cde-platform',
   projenrcTs: true,
@@ -8,5 +29,6 @@ const project = new typescript.TypeScriptProject({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
-});
-project.synth();
+})
+
+project.synth()
